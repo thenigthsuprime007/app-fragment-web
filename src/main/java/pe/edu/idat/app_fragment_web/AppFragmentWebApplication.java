9138369b6a0cb5337cea30adec3688ee28541fad
thenigthsuprime007 <carlos.flores.andres21@gmail.com>
2024-06-13 17:29:15 -0500

    initial commit

---
+++ src/main/java/pe/edu/idat/app_fragment_web/AppFragmentWebApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.app_fragment_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppFragmentWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppFragmentWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/idat/app_fragment_web/AppFragmentWebApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.app_fragment_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppFragmentWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
